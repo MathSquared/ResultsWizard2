c7b5aae51f930c8ecb9152d5b54d57708d2910cf
MathSquared <MathSquared@users.noreply.github.com>
2014-06-15 17:01:05 -0500

    Add payload retrieval methods to Command
    With ParamType and its standardization of Message parameter types, I can
    now create a coherent, interface-level way of retrieving the parameters
    to a given Message. Polymorphism FTW!

--- ResultsWizard2/src/mathsquared/resultswizard2/Command.java
+++ ResultsWizard2/src/mathsquared/resultswizard2/Command.java
@@ -4,6 +4,7 @@
 package mathsquared.resultswizard2;
 
 import java.io.Serializable;
+import java.util.Map;
 
 /**
  * Used to transmit a message between client and server. All implementors should define a serialVersionUID.
@@ -15,7 +16,23 @@ public interface Command extends Serializable {
     /**
      * Returns a Message representing the type of command that this object is carrying.
      * 
-     * @return the type of this Command
+     * @return the type of this Command, as a {@link Message}
      */
     public Message getType ();
+
+    /**
+     * Returns the String parameter to this Message, if applicable.
+     * 
+     * @return the String parameter to this Command, if supported by the {@linkplain #getType() relevant} {@link Message}
+     * @throws UnsupportedOperationException if this Message does not {@linkplain ParamType support} a String parameter
+     */
+    public String getStringPayload ();
+
+    /**
+     * Returns the Map<String, Slide[]> parameter to this Message, if applicable.
+     * 
+     * @return the Map<String, Slide[]> parameter to this Command, if supported by the {@linkplain #getType() relevant} {@link Message}
+     * @throws UnsupportedOperationException if this Message does not {@linkplain ParamType support} a Map<String, Slide[]> parameter
+     */
+    public Map<String, Slide[]> getStringSlidesPayload ();
 }
