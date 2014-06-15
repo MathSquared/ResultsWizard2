a337ae536eeb7cc68d72903450a05257af61a232
MathSquared <MathSquared@users.noreply.github.com>
2014-06-15 16:56:48 -0500

    Integrate Message with ParamType

--- ResultsWizard2/src/mathsquared/resultswizard2/Message.java
+++ ResultsWizard2/src/mathsquared/resultswizard2/Message.java
@@ -3,7 +3,6 @@
  */
 package mathsquared.resultswizard2;
 
-
 /**
  * Represents one of several possible messages sent between the GUI and display units of the application.
  * 
@@ -15,5 +14,25 @@ package mathsquared.resultswizard2;
  * 
  */
 public enum Message {
-    POISON, ADD, REMOVE, RETR_SLIDES, RESP_SLIDES, TICKER, RETR_TICKER, RESP_TICKER;
+    POISON(ParamType.NONE), ADD(ParamType.MAP_STRING_SLIDES), REMOVE(ParamType.STRING), RETR_SLIDES(ParamType.NONE), RESP_SLIDES(ParamType.MAP_STRING_SLIDES), TICKER(ParamType.STRING), RETR_TICKER(ParamType.NONE), RESP_TICKER(ParamType.STRING);
+
+    private ParamType param;
+
+    /**
+     * Constructs a Message with the given {@linkplain ParamType type for its parameters}.
+     * 
+     * @param param the type for the parameters to this Message
+     */
+    private Message (ParamType param) {
+        this.param = param;
+    }
+
+    /**
+     * Gets the {@link ParamType} carried by this Message.
+     * 
+     * @return a representation of the parameters allowed by this Message
+     */
+    public ParamType getParamType () {
+        return param;
+    }
 }
