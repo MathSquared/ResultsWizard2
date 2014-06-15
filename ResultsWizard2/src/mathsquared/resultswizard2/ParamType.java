e94cd7944a9b498bacbbb1994856bfd459862205
MathSquared <MathSquared@users.noreply.github.com>
2014-06-15 16:53:09 -0500

    Add ParamType
    I've filtered down the possible types of parameters that a Command can
    have to one of three:
    
    - no parameters
    - a String parameter
    - a Map<String, Slide[]> parameter
    
    This enum will be passed to the Message constructor and used by the
    Command interface when I add methods to retrieve message payload.
    Unsupported parameter types will probably either return null or throw
    UnsupportedOperationExceptions.

---
+++ ResultsWizard2/src/mathsquared/resultswizard2/ParamType.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package mathsquared.resultswizard2;
+
+/**
+ * Each message is defined to carry one of three parameter types--no parameters, a <code>String</code>, or a <code>Map&lt;String, Slide[]></code>. This enum represents all three options.
+ * 
+ * @author MathSquared
+ * 
+ */
+public enum ParamType {
+    NONE, STRING, MAP_STRING_SLIDES;
+}
