3cdc882a0e2a6c6c0c65553e3c07d92859847b40
MathSquared <MathSquared@users.noreply.github.com>
2014-06-16 20:31:09 -0500

    Add a test harness for ConnectionDetailsFrame (runs and waits)

---
+++ ResultsWizard2/test/mathsquared/resultswizard2/ConnectionDetailsFrameTest.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package mathsquared.resultswizard2;
+
+import org.junit.Test;
+
+/**
+ * @author MathSquared
+ * 
+ */
+public class ConnectionDetailsFrameTest {
+
+    /**
+     * Test method for {@link mathsquared.resultswizard2.ConnectionDetailsFrame#ConnectionDetailsFrame()}.
+     */
+    @Test
+    public void testConnectionDetailsFrame () {
+        ConnectionDetailsFrame cdf = new ConnectionDetailsFrame();
+        cdf.call();
+    }
+
+}
