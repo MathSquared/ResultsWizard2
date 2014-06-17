bc70c469c633652dc42dbcafcd5641e3c3a19c29
MathSquared <MathSquared@users.noreply.github.com>
2014-06-16 21:25:03 -0500

    Incorporate ConnectionDetailsFrame into the ResultsWizard2 class

--- ResultsWizard2/src/mathsquared/resultswizard2/ResultsWizard2.java
+++ ResultsWizard2/src/mathsquared/resultswizard2/ResultsWizard2.java
@@ -3,6 +3,13 @@
  */
 package mathsquared.resultswizard2;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.net.Socket;
+
 /**
  * Main class for the application; runs the administrator GUI that handles result entry.
  * 
@@ -15,8 +22,35 @@ public class ResultsWizard2 {
      * @param args the command-line arguments; unused
      */
     public static void main (String[] args) {
-        // TODO Auto-generated method stub
-
+        // Setup comms
+        ConnectionDetailsFrame cdf = new ConnectionDetailsFrame(true);
+        Socket sock = cdf.call();
+        InputStream istr;
+        OutputStream ostr;
+        if (sock != null) { // there is a socket; remote
+            try {
+                istr = sock.getInputStream();
+                ostr = sock.getOutputStream();
+            } catch (IOException e) {
+                System.out.println("ERROR: I/O error occurred when initializing network communication");
+                e.printStackTrace(System.out);
+                System.out.println("Exiting...");
+                System.exit(0);
+                return;
+            }
+        } else { // local
+            istr = new PipedInputStream();
+            ostr = new PipedOutputStream();
+            try {
+                new Display(new PipedInputStream((PipedOutputStream) ostr), new PipedOutputStream((PipedInputStream) istr));
+            } catch (IOException e) {
+                System.out.println("ERROR: I/O error occurred when initializing display");
+                e.printStackTrace(System.out);
+                System.out.println("Exiting...");
+                System.exit(0);
+                return;
+            }
+        }
     }
 
 }
