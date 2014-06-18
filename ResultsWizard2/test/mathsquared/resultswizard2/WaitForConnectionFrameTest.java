c46b3a180fd0da015738fa26c3d2187b354fa913
MathSquared <MathSquared@users.noreply.github.com>
2014-06-18 15:50:32 -0500

    Add WaitForConnectionFrameTest

---
+++ ResultsWizard2/test/mathsquared/resultswizard2/WaitForConnectionFrameTest.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package mathsquared.resultswizard2;
+
+import org.junit.Test;
+
+/**
+ * @author MathSquared
+ * 
+ */
+public class WaitForConnectionFrameTest {
+
+    /**
+     * Test method for {@link mathsquared.resultswizard2.WaitForConnectionFrame#WaitForConnectionFrame()}.
+     */
+    @Test
+    public void testWaitForConnectionFrame () {
+        WaitForConnectionFrame wfcf = new WaitForConnectionFrame();
+        wfcf.call();
+    }
+
+}
