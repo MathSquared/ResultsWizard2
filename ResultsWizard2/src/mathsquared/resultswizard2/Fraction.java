97bdf326c4c74d679b4c86439ad6136ff103a5d9
MathSquared <MathSquared@users.noreply.github.com>
2014-06-27 22:35:54 -0500

    Create the Fraction class
    I want to have very high precision when handling sweepstakes points.
    Since some mechanisms use averages to handle sweeps in situations where
    competitors are tied, this could lead to rational non-integer points.
    So, I will use the Fraction class for all sweepstakes point arithmetic.
    
    This object will represent a proper fraction; it is intended to be
    paired with an int that represents the unit portion. It will support
    additions that would create an improper fraction, and provides methods
    to retrieve the unit portion. It will also work properly when using
    multiple additions in a row, even when this would lead to an improper
    fraction.

---
+++ ResultsWizard2/src/mathsquared/resultswizard2/Fraction.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package mathsquared.resultswizard2;
+
+/**
+ * An immutable object that represents a proper fraction. Can support improper fractions intermittently as well.
+ * 
+ * @author MathSquared
+ * 
+ */
+public class Fraction {
+
+}
