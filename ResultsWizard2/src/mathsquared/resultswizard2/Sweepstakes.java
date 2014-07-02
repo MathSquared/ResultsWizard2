ee7a8628f9dfffb114852014f6f1a57a1ad6a558
MathSquared <MathSquared@users.noreply.github.com>
2014-07-01 21:53:50 -0500

    Add Sweepstakes and assignPoints
    This class handles figuring out sweepstakes totals. It takes simply the
    number of people in each place and skips places when needed--so, a
    two-way tie for first followed by a third place competitor would be
    passed into quantities as {2, 1}.

---
+++ ResultsWizard2/src/mathsquared/resultswizard2/Sweepstakes.java
@@ -0,0 +1,87 @@
+/**
+ * 
+ */
+package mathsquared.resultswizard2;
+
+/**
+ * Given a set of event results, determines the amount of sweepstakes points to assign to each contestant.
+ * 
+ * @author MathSquared
+ * 
+ */
+public class Sweepstakes {
+    public Fraction[] assignPoints (int[] quantities, int[] spec, TiePlaceAssignment tiePlaceM, SweepstakesAssignment sweepsM) {
+        if (sweepsM == SweepstakesAssignment.CUSTOM) {
+            return null; // the app should be prompting--leave that to the GUI code, not the back-end
+        }
+
+        Fraction[] ret = new Fraction[quantities.length];
+
+        int currPlace = 1; // tracks which place we're on, since multiple places are condensed into one entry in quantities
+        for (int i = 0; i < quantities.length; i++) {
+            int newPlace = currPlace + quantities[i] - 1; // fencepost; quantity of 1 is from place 1 to place 1, not 1 to 2
+            Fraction sweeps = new Fraction(0);
+            switch (sweepsM) {
+            case TIE_PLACE:
+                sweeps = new Fraction(spec[tiePlaceM.assignPlace(currPlace, newPlace)]);
+                break;
+            case TOP:
+                sweeps = new Fraction(spec[currPlace]);
+                break;
+            case BOTTOM:
+                sweeps = new Fraction(spec[newPlace]);
+                break;
+            case MID_ROUND_BETTER:
+                sweeps = new Fraction(spec[currPlace + (newPlace - currPlace) / 2]); // division truncates, so this rounds toward adding less
+                break;
+            case MID_ROUND_WORSE:
+                sweeps = new Fraction(spec[newPlace - (newPlace - currPlace) / 2]);
+                break;
+            case AVERAGE: // take the average of all teh things
+                int total = 0;
+                int count = 0;
+                for (int j = currPlace; j <= newPlace; j++) {
+                    total += (j < spec.length) ? spec[j] : 0; // we keep going in an overrun--we still need to add the other places
+                    count++;
+                }
+                sweeps = new Fraction(total, count);
+                break;
+            case AVERAGE_ALL:
+                int total1 = 0; // Ignore the numbers after the variables--they're duplicated in scope from case AVERAGE above, so they need to be distinguished.
+                int count1 = 0;
+                for (int j = currPlace; j <= newPlace && j < spec.length; j++) { // here, we stop the loop early since we ignore all nonexistent specifications
+                    total1 += spec[j];
+                    count1++;
+                }
+                sweeps = new Fraction(total1, count1);
+                break;
+            case AVERAGE_ADJUSTED:
+                int total2 = 0;
+                int count2 = 0;
+                for (int j = currPlace + 1; j < newPlace; j++) { // less than in condition intentional to exclude last place
+                    total2 += (j < spec.length) ? spec[j] : 0;
+                    count2++;
+                }
+                sweeps = new Fraction(total2, count2);
+                break;
+            case MEDIAN:
+                int placeDiff = quantities[i] - 1; // difference between currPlace and newPlace
+                int halfDiff = placeDiff / 2;
+                if (placeDiff % 2 != 0) { // even number of places, due to fenceposting (1 - 1 = 0, but is 1 place)
+                    // average two middle ones
+                    sweeps = new Fraction(spec[currPlace + halfDiff] + spec[currPlace + halfDiff + 1], 2);
+                } else { // odd number of places
+                    sweeps = new Fraction(spec[currPlace + halfDiff]);
+                }
+                break;
+            default:
+                sweeps = null;
+            }
+
+            ret[i] = sweeps;
+            currPlace += quantities[i]; // next set of places; this time, we want to move past the range we're in
+        }
+
+        return ret;
+    }
+}
