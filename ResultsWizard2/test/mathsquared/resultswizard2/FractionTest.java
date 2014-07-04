9b69375397b2a40e3ead6c5f88e54feee1cbf4f5
MathSquared <MathSquared@users.noreply.github.com>
2014-07-04 00:05:26 -0500

    Generate FractionTest
    This will be a fun test suite to implement...

---
+++ ResultsWizard2/test/mathsquared/resultswizard2/FractionTest.java
@@ -0,0 +1,94 @@
+package mathsquared.resultswizard2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FractionTest {
+
+    @Test
+    public void testFractionInt () {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testFractionIntInt () {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testExtractUnit () {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetImproperNumerator () {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testNegative () {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testMultiplyInt () {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testMultiplyFraction () {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testDivideInt () {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testDivideFraction () {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testPow () {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testReciprocal () {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testAddInt () {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testAddFraction () {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSubtractInt () {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSubtractFraction () {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testToDouble () {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testEqualsObject () {
+        fail("Not yet implemented");
+    }
+
+}
