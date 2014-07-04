9a0eab92dad46a000088e989d35358802b25ac1d
MathSquared <MathSquared@users.noreply.github.com>
2014-07-04 00:06:55 -0500

    Regenerate FractionTest with Javadoc templates

--- ResultsWizard2/test/mathsquared/resultswizard2/FractionTest.java
+++ ResultsWizard2/test/mathsquared/resultswizard2/FractionTest.java
@@ -1,91 +1,149 @@
+/**
+ * 
+ */
 package mathsquared.resultswizard2;
 
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+/**
+ * @author MathSquared
+ *
+ */
 public class FractionTest {
 
+    /**
+     * Test method for {@link mathsquared.resultswizard2.Fraction#Fraction(int)}.
+     */
     @Test
     public void testFractionInt () {
         fail("Not yet implemented");
     }
 
+    /**
+     * Test method for {@link mathsquared.resultswizard2.Fraction#Fraction(int, int)}.
+     */
     @Test
     public void testFractionIntInt () {
         fail("Not yet implemented");
     }
 
+    /**
+     * Test method for {@link mathsquared.resultswizard2.Fraction#extractUnit()}.
+     */
     @Test
     public void testExtractUnit () {
         fail("Not yet implemented");
     }
 
+    /**
+     * Test method for {@link mathsquared.resultswizard2.Fraction#getImproperNumerator()}.
+     */
     @Test
     public void testGetImproperNumerator () {
         fail("Not yet implemented");
     }
 
+    /**
+     * Test method for {@link mathsquared.resultswizard2.Fraction#negative()}.
+     */
     @Test
     public void testNegative () {
         fail("Not yet implemented");
     }
 
+    /**
+     * Test method for {@link mathsquared.resultswizard2.Fraction#multiply(int)}.
+     */
     @Test
     public void testMultiplyInt () {
         fail("Not yet implemented");
     }
 
+    /**
+     * Test method for {@link mathsquared.resultswizard2.Fraction#multiply(mathsquared.resultswizard2.Fraction)}.
+     */
     @Test
     public void testMultiplyFraction () {
         fail("Not yet implemented");
     }
 
+    /**
+     * Test method for {@link mathsquared.resultswizard2.Fraction#divide(int)}.
+     */
     @Test
     public void testDivideInt () {
         fail("Not yet implemented");
     }
 
+    /**
+     * Test method for {@link mathsquared.resultswizard2.Fraction#divide(mathsquared.resultswizard2.Fraction)}.
+     */
     @Test
     public void testDivideFraction () {
         fail("Not yet implemented");
     }
 
+    /**
+     * Test method for {@link mathsquared.resultswizard2.Fraction#pow(int)}.
+     */
     @Test
     public void testPow () {
         fail("Not yet implemented");
     }
 
+    /**
+     * Test method for {@link mathsquared.resultswizard2.Fraction#reciprocal()}.
+     */
     @Test
     public void testReciprocal () {
         fail("Not yet implemented");
     }
 
+    /**
+     * Test method for {@link mathsquared.resultswizard2.Fraction#add(int)}.
+     */
     @Test
     public void testAddInt () {
         fail("Not yet implemented");
     }
 
+    /**
+     * Test method for {@link mathsquared.resultswizard2.Fraction#add(mathsquared.resultswizard2.Fraction)}.
+     */
     @Test
     public void testAddFraction () {
         fail("Not yet implemented");
     }
 
+    /**
+     * Test method for {@link mathsquared.resultswizard2.Fraction#subtract(int)}.
+     */
     @Test
     public void testSubtractInt () {
         fail("Not yet implemented");
     }
 
+    /**
+     * Test method for {@link mathsquared.resultswizard2.Fraction#subtract(mathsquared.resultswizard2.Fraction)}.
+     */
     @Test
     public void testSubtractFraction () {
         fail("Not yet implemented");
     }
 
+    /**
+     * Test method for {@link mathsquared.resultswizard2.Fraction#toDouble()}.
+     */
     @Test
     public void testToDouble () {
         fail("Not yet implemented");
     }
 
+    /**
+     * Test method for {@link mathsquared.resultswizard2.Fraction#equals(java.lang.Object)}.
+     */
     @Test
     public void testEqualsObject () {
         fail("Not yet implemented");
