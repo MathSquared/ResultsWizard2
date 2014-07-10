7f1cde343a4c3db2d0674a7798517a40a6bcd496
MathSquared <MathSquared@users.noreply.github.com>
2014-07-10 14:35:44 -0500

    Test against the right known-good array [NOBODY SAW THAT]
    Nobody saw that last commit, in which I totally didn't forget to change
    from parsed to parsed2.

--- ResultsWizard2/test/mathsquared/resultswizard2/SyntaxParserTest.java
+++ ResultsWizard2/test/mathsquared/resultswizard2/SyntaxParserTest.java
@@ -50,7 +50,7 @@ public class SyntaxParserTest {
         int[] parsed = SyntaxParser.parseIntegerList("1 2,3 4.5$");
         int[] parsed2 = SyntaxParser.parseIntegerList("1, 2: 3, ");
         assertTrue("integer list: five integers, various separators, trailing", Arrays.equals(parsed, new int[] {1, 2, 3, 4, 5}));
-        assertTrue("integer list: various multiple-character separators, trailing", Arrays.equals(parsed, new int[] {1, 2, 3}));
+        assertTrue("integer list: various multiple-character separators, trailing", Arrays.equals(parsed2, new int[] {1, 2, 3}));
     }
 
 }
