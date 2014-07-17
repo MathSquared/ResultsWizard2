acf86110d5704cb0345d0d663be1a136599ed1dd
MathSquared <MathSquared@users.noreply.github.com>
2014-07-16 23:14:30 -0500

    Add SweepstakesTest (skeleton)
    This is going to be good.

---
+++ ResultsWizard2/test/mathsquared/resultswizard2/SweepstakesTest.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package mathsquared.resultswizard2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author MathSquared
+ *
+ */
+public class SweepstakesTest {
+
+    /**
+     * Test method for {@link mathsquared.resultswizard2.Sweepstakes#assignPoints(int[], int[], mathsquared.resultswizard2.TiePlaceAssignment, mathsquared.resultswizard2.SweepstakesAssignment)}.
+     */
+    @Test
+    public void testAssignPoints () {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link mathsquared.resultswizard2.Sweepstakes#linkSweepstakes(java.lang.String[][], mathsquared.resultswizard2.Fraction[])}.
+     */
+    @Test
+    public void testLinkSweepstakes () {
+        fail("Not yet implemented");
+    }
+
+}
