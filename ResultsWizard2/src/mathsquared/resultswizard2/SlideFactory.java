54b7b2c926dccb79ea0d1f9046f35fe721778aa5
MathSquared <MathSquared@users.noreply.github.com>
2014-07-22 00:06:32 -0500

    SlideFactory.generate now returns SlideList

--- ResultsWizard2/src/mathsquared/resultswizard2/SlideFactory.java
+++ ResultsWizard2/src/mathsquared/resultswizard2/SlideFactory.java
@@ -11,12 +11,12 @@ package mathsquared.resultswizard2;
  */
 public interface SlideFactory {
     /**
-     * Generates an array of {@link Slide}s representing the given event results, in the order that they should be projected.
+     * Generates a {@linkplain SlideList list} of {@link Slide}s representing the given event results, in the order that they should be projected.
      * 
      * @param er the {@link EventResults} for which to generate <code>Slide</code>s
      * @param width the width that these <code>Slides</code> should use to draw themselves
      * @param height the height that these <code>Slides</code> should use to draw themselves
-     * @return an array of <code>Slide</code>s that, when projected in the order given, represent the given <code>EventResults</code>
+     * @return an list of <code>Slide</code>s that, when projected in the order given, represent the given <code>EventResults</code>
      */
-    public Slide[] generate (EventResults er, int width, int height);
+    public SlideList generate (EventResults er, int width, int height);
 }
