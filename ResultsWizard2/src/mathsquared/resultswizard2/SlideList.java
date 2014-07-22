8291e5023d3af272ecd545bd737789df2064eb90
MathSquared <MathSquared@users.noreply.github.com>
2014-07-22 00:05:51 -0500

    Add skeletal SlideList

---
+++ ResultsWizard2/src/mathsquared/resultswizard2/SlideList.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package mathsquared.resultswizard2;
+
+import java.util.List;
+
+/**
+ * Represents a list of {@link Slide}s. Subinterfaces will carry different types of metadata (e.g. {@linkplain EventResults event results}) that are represented by the slides.
+ * 
+ * @author MathSquared
+ * 
+ */
+public interface SlideList extends List<Slide> {
+
+}
