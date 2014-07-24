b7033bbc8fa7fdad25ee8ab5efb6fb21d423df30
MathSquared <MathSquared@users.noreply.github.com>
2014-07-24 11:51:22 -0500

    Add FontUtils and shrinkFontForWidth

---
+++ ResultsWizard2/src/mathsquared/resultswizard2/FontUtils.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package mathsquared.resultswizard2;
+
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+
+/**
+ * Contains utility methods for working with fonts.
+ * 
+ * @author MathSquared
+ * 
+ */
+public class FontUtils {
+    public Font shrinkFontForWidth (Font base, String text, int desiredWidth, Graphics getMetrics) {
+        FontMetrics metrics = getMetrics.getFontMetrics(base);
+        while (metrics.stringWidth(text) > desiredWidth) {
+            base = base.deriveFont((float) base.getSize() - 1);
+        }
+        return base;
+    }
+}
