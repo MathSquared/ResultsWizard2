9de04716cb84724a5749adb9156b7c45a629efa8
MathSquared <MathSquared@users.noreply.github.com>
2014-07-24 00:39:05 -0500

    Add the Chunks to BuildableStackedSlide

--- ResultsWizard2/src/mathsquared/resultswizard2/BuildableStackedSlide.java
+++ ResultsWizard2/src/mathsquared/resultswizard2/BuildableStackedSlide.java
@@ -3,6 +3,7 @@
  */
 package mathsquared.resultswizard2;
 
+import java.awt.Font;
 import java.awt.Graphics;
 
 /**
@@ -16,6 +17,84 @@ public class BuildableStackedSlide implements Slide {
     private int width;
     private int height;
     private ColorScheme color;
+
+    // CHUNK CLASSES //
+
+    // These represent the different elements that can be created. //
+
+    private interface Chunk {
+        /**
+         * Returns the maximum possible vertical size that a chunk of this type can take up with the given parameters. Maximum possible means that none of the fonts are shrunk due to lack of horizontal space.
+         * 
+         * @return the maximum possible vertical size of this chunk, in pixels
+         */
+        public int getMaxVertSize ();
+    }
+
+    private class SpacerChunk implements Chunk {
+        public int px;
+
+        public int getMaxVertSize () {
+            return px;
+        }
+    }
+
+    private class TextChunk implements Chunk {
+        public String str;
+        public Font font;
+
+        public int getMaxVertSize () {
+            return g.getFontMetrics(font).getHeight();
+        }
+    }
+
+    private class ThreeTextChunk implements Chunk {
+        public String str1;
+        public Font font1;
+        public String str2;
+        public Font font2;
+        public String str3;
+        public Font font3;
+
+        public int getMaxVertSize () {
+            int h1 = g.getFontMetrics(font1).getHeight();
+            int h2 = g.getFontMetrics(font2).getHeight();
+            int h3 = g.getFontMetrics(font3).getHeight();
+
+            int mx = h1;
+            mx = (h2 > mx) ? h2 : mx;
+            mx = (h3 > mx) ? h3 : mx;
+
+            return mx;
+        }
+    }
+
+    private class FourTextChunk implements Chunk {
+        public String str1;
+        public Font font1;
+        public String str2;
+        public Font font2;
+        public String str3;
+        public Font font3;
+        public String str4;
+        public Font font4;
+
+        public int getMaxVertSize () {
+            int h1 = g.getFontMetrics(font1).getHeight();
+            int h2 = g.getFontMetrics(font2).getHeight();
+            int h3 = g.getFontMetrics(font3).getHeight();
+            int h4 = g.getFontMetrics(font4).getHeight();
+
+            int mx = h1;
+            mx = (h2 > mx) ? h2 : mx;
+            mx = (h3 > mx) ? h3 : mx;
+            mx = (h4 > mx) ? h4 : mx;
+
+            return mx;
+        }
+    }
+
+    // LOGIC //
 
     public BuildableStackedSlide (Graphics g, int width, int height, ColorScheme color) {
         this.g = g;
