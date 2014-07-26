625fd3259d147a010600a689d32cfe68b84fb80f
MathSquared <MathSquared@users.noreply.github.com>
2014-07-25 23:08:39 -0500

    Document FontUtils.shrinkFontForWidth

--- ResultsWizard2/src/mathsquared/resultswizard2/FontUtils.java
+++ ResultsWizard2/src/mathsquared/resultswizard2/FontUtils.java
@@ -14,6 +14,19 @@ import java.awt.Graphics;
  * 
  */
 public class FontUtils {
+    /**
+     * Finds the smallest font size that will fit a string within a given width.
+     * 
+     * <p>
+     * Specifically, returns a {@link Font} <code>ret</code> with a size less than its current size such that <code>getMetrics.getFontMetrics(ret).stringWidth(text) &lt;= desiredWidth</code>.
+     * </p>
+     * 
+     * @param base the base font which should be shrunk (not modified)
+     * @param text the string to fit within the given width
+     * @param desiredWidth the maximum possible width of the text, in pixels
+     * @param getMetrics a {@link Graphics} object which should be used to obtain {@link FontMetrics} for the given font
+     * @return a Font identical to <code>base</code> except possibly with a different font size as above
+     */
     public static Font shrinkFontForWidth (Font base, String text, int desiredWidth, Graphics getMetrics) {
         FontMetrics metrics = getMetrics.getFontMetrics(base);
         while (metrics.stringWidth(text) > desiredWidth) {
