9f013de3bd84fead8ba1cde288f9626a915fa32c
MathSquared <MathSquared@users.noreply.github.com>
2014-08-04 23:17:26 -0500

    MapSortingUtils.sortValue: Add thenByKey
    Don't you just LOVE unchecked casts? I'm sure the Java API is chock-full
    of them for handling situations like this one.
    
    *Billy Mays strides in, confidently as always*
    
    BUT WAIT--THERE'S MORE!
    
    Call NOW, and we'll add a free reading of Chapter 5 of the Java Language
    Specification, with a SPECIAL focus on unchecked conversions! That's
    right--100% guaranteed to blow your mind, and it's yours right now at no
    extra charge! That's a mind-blowing code segment and its even more
    mind-blowing specification, yours TODAY for just $19.99! Call NOW!

--- ResultsWizard2/src/mathsquared/resultswizard2/MapSortingUtils.java
+++ ResultsWizard2/src/mathsquared/resultswizard2/MapSortingUtils.java
@@ -30,15 +30,18 @@ public class MapSortingUtils {
      * 
      * @param toSort the map to sort
      * @param descending true if the highest values should sort to the beginning
+     * @param thenByKey true if values that compare as equal should then be sorted by the values of their keys; keys are always sorted ascending, and will compare as equal if they do not implement Comparable
      * @return a new Map with its keys sorted in the natural order of their associated values
      */
-    public static <K, V extends Comparable<? super V>> Map<K, V> sortValue (Map<K, V> toSort, boolean descending) {
+    public static <K, V extends Comparable<? super V>> Map<K, V> sortValue (Map<K, V> toSort, boolean descending, final boolean thenByKey) {
         // Set up ascending or descending.
         final int mult = (descending) ? -1 : 1; // if descending, multiply all comparisons by -1
 
         // HEAVILY based on http://stackoverflow.com/a/2581754/1979005
         List<Map.Entry<K, V>> entries = new LinkedList<Map.Entry<K, V>>(toSort.entrySet());
         Collections.sort(entries, new Comparator<Map.Entry<K, V>>() {
+            // Horrid code to cope with sorting by key
+            @SuppressWarnings("unchecked")
             public int compare (Map.Entry<K, V> one, Map.Entry<K, V> two) {
                 if (one == null) { // sort nulls less than everything else (before in ascending, after in descending)
                     if (two == null) {
@@ -50,7 +53,21 @@ public class MapSortingUtils {
                     if (two == null) { // (*, null)
                         return 1 * mult;
                     } else {
-                        return mult * one.getValue().compareTo(two.getValue());
+                        int valComp = mult * one.getValue().compareTo(two.getValue());
+                        if (!thenByKey) { // Detect if we should sort by key
+                            return valComp;
+                        } else if (valComp != 0) {
+                            return valComp;
+                        } else if (!(one.getKey() instanceof Comparable<?>) || !(two.getKey() instanceof Comparable<?>)) { // cannot be compared
+                            return 0;
+                        } else {
+                            try {
+                                return mult * ((Comparable) one.getKey()).compareTo(two.getKey());
+                            } catch (ClassCastException e) {
+                                // if we can't compare, return 0
+                                return 0;
+                            }
+                        }
                     }
                 }
             }
