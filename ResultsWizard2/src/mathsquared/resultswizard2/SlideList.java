79f5077d3645765564968a033ca2806aa2622c67
MathSquared <MathSquared@users.noreply.github.com>
2014-08-19 14:39:05 -0500

    Add renderSlides to interface SlideList
    This will be used to let the ProtocolSelector automatically render
    slides to a certain size.

--- ResultsWizard2/src/mathsquared/resultswizard2/SlideList.java
+++ ResultsWizard2/src/mathsquared/resultswizard2/SlideList.java
@@ -13,5 +13,15 @@ import java.util.List;
  * 
  */
 public interface SlideList extends List<Slide>, Serializable {
-
+    /**
+     * Modifies the Slides carried by this SlideList to display using the given width and height. This method can change any aspect of the slides, including their number, amount, and relative ordering; it is as if the SlideList was reinitialized from its raw data.
+     * 
+     * <p>
+     * Implementors may impose their own restrictions on the possible arguments to this method.
+     * </p>
+     * 
+     * @param width the width of the slides, in pixels
+     * @param height the height of the slides, in pixels
+     */
+    public void renderSlides (int width, int height);
 }
