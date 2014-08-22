08482a864687e03151f0e4e5b2ea06da095fc56d
MathSquared <MathSquared@users.noreply.github.com>
2014-08-21 23:11:11 -0500

    QC: Only send a command if it is non-null

--- ResultsWizard2/src/mathsquared/resultswizard2/QueueCommunicator.java
+++ ResultsWizard2/src/mathsquared/resultswizard2/QueueCommunicator.java
@@ -44,8 +44,9 @@ public class QueueCommunicator implements Runnable {
             if (pop != null && pop instanceof Command) {
                 Command cmd = (Command) pop;
                 Command send = comm.processMessage(cmd); // Get the command to send back
-                tx.add(send);
-
+                if (send != null) { // Only send the command if there's actually something to send (SQProxy's queues choke on null)
+                    tx.add(send);
+                }
                 // Ensure that we're not aborting (POISON or XMIT_ERROR_RESTART invalidate the streams)
                 if (cmd.getType().equals(Message.POISON) || cmd.getType().equals(Message.XMIT_ERROR_RESTART)) {
                     aborted = true;
