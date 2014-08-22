4173e363df5aeecbc797fb80803c61660c3f0a65
MathSquared <MathSquared@users.noreply.github.com>
2014-08-21 23:05:36 -0500

    Add QueueCommunicator
    This will be run in a thread by the display to continually receive
    commands from the queues (which will be maintained by a StreamQueueProxy
    in its own thread).
    
    As a side note, non-blocking IO requires a lot of threads.

---
+++ ResultsWizard2/src/mathsquared/resultswizard2/QueueCommunicator.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package mathsquared.resultswizard2;
+
+import java.util.Queue;
+
+/**
+ * Feeds data from a queue to another object, and from the object to another queue. Currently, the only supported object is a {@link ProtocolSelector}.
+ * 
+ * @author MathSquared
+ * 
+ */
+public class QueueCommunicator implements Runnable {
+    private Queue rx; // receiving
+    private Queue tx; // sending
+    private ProtocolSelector comm;
+
+    public QueueCommunicator (Queue rx, Queue tx, ProtocolSelector comm) {
+        this.rx = rx;
+        this.tx = tx;
+        this.comm = comm;
+    }
+
+    public void run () {
+        boolean aborted = false;
+        while (!aborted) {
+            // Wait until we have data
+            while (rx.size() == 0) {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    // ignore
+                }
+            }
+
+            // Process the data
+
+            Object pop = rx.poll();
+
+            // This is an object stream, so we must check and cast (and ensure that we're not about to NPE, just to be safe)
+            if (pop != null && pop instanceof Command) {
+                Command cmd = (Command) pop;
+                Command send = comm.processMessage(cmd); // Get the command to send back
+                tx.add(send);
+
+                // Ensure that we're not aborting (POISON or XMIT_ERROR_RESTART invalidate the streams)
+                if (cmd.getType().equals(Message.POISON) || cmd.getType().equals(Message.XMIT_ERROR_RESTART)) {
+                    aborted = true;
+                }
+            }
+        }
+    }
+}
