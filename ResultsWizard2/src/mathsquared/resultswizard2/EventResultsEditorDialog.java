8c6f0e5e4c96db073c6be228bc2ac0c8695d33ef
MathSquared <MathSquared@users.noreply.github.com>
2014-08-22 00:31:03 -0500

    ERED: Add an actual contentPane

--- ResultsWizard2/src/mathsquared/resultswizard2/EventResultsEditorDialog.java
+++ ResultsWizard2/src/mathsquared/resultswizard2/EventResultsEditorDialog.java
@@ -1,6 +1,10 @@
 package mathsquared.resultswizard2;
 
+import java.awt.BorderLayout;
+
 import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
 /**
  * Runs the GUI by which an event administrator can input or edit {@link EventResults}.
@@ -9,6 +13,8 @@ import javax.swing.JDialog;
  * 
  */
 public class EventResultsEditorDialog extends JDialog {
+
+    private JPanel contentPane;
 
     /**
      * Create the dialog.
@@ -19,6 +25,10 @@ public class EventResultsEditorDialog extends JDialog {
         setResizable(false);
         setTitle("Edit Event Results");
         setBounds(100, 100, 450, 300);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        contentPane.setLayout(new BorderLayout(0, 0));
+        setContentPane(contentPane);
 
     }
 
