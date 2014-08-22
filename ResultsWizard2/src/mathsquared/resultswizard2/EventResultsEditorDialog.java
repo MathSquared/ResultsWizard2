f2fa0531ef610bec41bf4a50df29a6267b41dc8b
MathSquared <MathSquared@users.noreply.github.com>
2014-08-22 00:20:10 -0500

    Add a bare-bones EventResultsEditorDialog
    This is a dialog because I intend it to be modal--the user should be
    unable to interact with the rest of the app while the dialog is open.
    Failure to adhere to this could lead to strange behaviors--users opening
    multiple EREDs that become out-of-sync for each other, users minimizing
    EREDs...

---
+++ ResultsWizard2/src/mathsquared/resultswizard2/EventResultsEditorDialog.java
@@ -0,0 +1,21 @@
+package mathsquared.resultswizard2;
+
+import javax.swing.JDialog;
+
+/**
+ * Runs the GUI by which an event administrator can input or edit {@link EventResults}.
+ * 
+ * @author MathSquared
+ * 
+ */
+public class EventResultsEditorDialog extends JDialog {
+
+    /**
+     * Create the dialog.
+     */
+    public EventResultsEditorDialog () {
+        setBounds(100, 100, 450, 300);
+
+    }
+
+}
