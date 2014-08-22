6935edb1d2dc29192dfe57ade1bbaf5ed945419c
MathSquared <MathSquared@users.noreply.github.com>
2014-08-21 23:19:22 -0500

    Document QueueCommunicator

--- ResultsWizard2/src/mathsquared/resultswizard2/QueueCommunicator.java
+++ ResultsWizard2/src/mathsquared/resultswizard2/QueueCommunicator.java
@@ -18,12 +18,30 @@ public class QueueCommunicator implements Runnable {
 
     public static final long SLEEP_TIME = 100; // time to sleep when rx is empty
 
+    /**
+     * Creates a QueueCommunicator communicating between two given queues and a given {@link CommandProcessor}. Note that the QueueCommunicator must be {@linkplain #run() run} to have any effect.
+     * 
+     * @param rx a queue from which to obtain messages received from the outside
+     * @param tx a queue to which to send messages intended for return to the sender of any messages popped from <code>rx</code>
+     * @param comm a CommandProcessor that will receive the commands from <code>rx</code> and generate new commands to send to <code>tx</code>
+     */
     public QueueCommunicator (Queue rx, Queue tx, CommandProcessor comm) {
         this.rx = rx;
         this.tx = tx;
         this.comm = comm;
     }
 
+    /**
+     * Runs the QueueCommunicator until aborted.
+     * 
+     * <p>
+     * A QueueCommunicator is aborted when a Command is removed from the queue given by <code>rx</code> whose {@link Command#getType() getType()} method returns {@link Message#POISON} or {@link Message#XMIT_ERROR_RESTART}. If the CommandProcessor designates a response to these commands, it will be sent prior to the QueueCommunicator being aborted.
+     * </p>
+     * 
+     * <p>
+     * After being aborted, a QueueCommunicator may be restarted, with the same queues, by calling this method again.
+     * </p>
+     */
     public void run () {
         boolean aborted = false;
         while (!aborted) {
