9f1457ed3d63e6af59364d0c1ab7c6da9e1a2e75
MathSquared <MathSquared@users.noreply.github.com>
2014-09-06 22:33:19 -0500

    Add EditorTableModel
    This will be used by ERED and the forthcoming SED
    (SweepstakesEditorDialog) in order to allow the user to edit data.
    
    ----
    
    WHY I HAVE BEEN INACTIVE
    ========================
    
    First, I've been in the process of migrating to a new PC. I've had to
    transfer all of my configuration, etc. to this new computer. Having this
    project on Github simplified this somewhat, but it still took time to
    transfer e.g. my Eclipse configuration.
    
    Second, classes have started and I thus have less time in general.

---
+++ ResultsWizard2/src/mathsquared/resultswizard2/EditorTableModel.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package mathsquared.resultswizard2;
+
+import javax.swing.table.TableModel;
+
+/**
+ * Represents data that can be edited in one or more stages and then transformed into another object.
+ * 
+ * @author MathSquared
+ * @param <T> the type of data that can be obtained from this model using {@link #getResult()}
+ *
+ */
+public interface EditorTableModel<T> extends TableModel {
+	/**
+	 * Returns the number of stages used by this editor.
+	 * 
+	 * @return a number indicating the number of stages; this number must always be greater than or equal to 1
+	 */
+	public int numStages ();
+	
+	/**
+	 * Returns the current stage.
+	 * 
+	 * @return a zero-based index for the current stage; this is guaranteed to be <code>&gt;= 0</code> and <code>&lt; {@link #numStages()}</code>
+	 */
+	public int currentStage ();
+	
+	/**
+	 * Moves to the next stage.
+	 * 
+	 * @throws IllegalStateException if we are at the last stage
+	 */
+	public void nextStage ();
+	
+	/**
+	 * Moves to the previous stage.
+	 * 
+	 * @throws IllegalStateException if we are at the first stage
+	 */
+	public void previousStage ();
+	
+	/**
+	 * Obtains the current result of this model. This only works if we are at the last stage.
+	 * 
+	 * @return an object representing the data stored in this model
+	 * @throws IllegalStateException if we are not at the last stage
+	 */
+	public T getResult ();
+}
