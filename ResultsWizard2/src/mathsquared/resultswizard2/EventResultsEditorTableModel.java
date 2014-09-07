badb54678fb906004473f7d44f43b0e7f6681697
MathSquared <MathSquared@users.noreply.github.com>
2014-09-06 22:50:08 -0500

    ERETM extends AbstractTableModel

--- ResultsWizard2/src/mathsquared/resultswizard2/EventResultsEditorTableModel.java
+++ ResultsWizard2/src/mathsquared/resultswizard2/EventResultsEditorTableModel.java
@@ -3,40 +3,22 @@
  */
 package mathsquared.resultswizard2;
 
-import javax.swing.event.TableModelListener;
+import javax.swing.table.AbstractTableModel;
 
 /**
  * Allows for the editing of one {@linkplain Facet facet} (individual, team, or special) of {@link EventResults}.
  * 
  * @author MathSquared
- *
+ * 
  */
-public class EventResultsEditorTableModel implements EditorTableModel<EventResults> {
+public class EventResultsEditorTableModel extends AbstractTableModel implements EditorTableModel<EventResults> {
 	private EventResults buffer; // this holds the current state of the results, as edited by the user
 	private Facet facet; // the facet edited by this editor
-
-	@Override
-	public void addTableModelListener(TableModelListener arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public Class<?> getColumnClass(int arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 	@Override
 	public int getColumnCount() {
 		// TODO Auto-generated method stub
 		return 0;
-	}
-
-	@Override
-	public String getColumnName(int arg0) {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 	@Override
@@ -46,27 +28,9 @@ public class EventResultsEditorTableModel implements EditorTableModel<EventResul
 	}
 
 	@Override
-	public Object getValueAt(int arg0, int arg1) {
+	public Object getValueAt(int rowIndex, int columnIndex) {
 		// TODO Auto-generated method stub
 		return null;
-	}
-
-	@Override
-	public boolean isCellEditable(int arg0, int arg1) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public void removeTableModelListener(TableModelListener arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void setValueAt(Object arg0, int arg1, int arg2) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
@@ -84,13 +48,13 @@ public class EventResultsEditorTableModel implements EditorTableModel<EventResul
 	@Override
 	public void nextStage() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void previousStage() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
