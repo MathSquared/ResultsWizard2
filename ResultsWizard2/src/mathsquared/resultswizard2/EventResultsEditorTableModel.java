46ccead47c82dfc2b70a1b5c1dde7c9592533c19
MathSquared <MathSquared@users.noreply.github.com>
2014-09-06 22:53:14 -0500

    ERETM formatting
    I thought this would be taken care of.
    
    *checks Eclipse settings*

--- ResultsWizard2/src/mathsquared/resultswizard2/EventResultsEditorTableModel.java
+++ ResultsWizard2/src/mathsquared/resultswizard2/EventResultsEditorTableModel.java
@@ -12,54 +12,54 @@ import javax.swing.table.AbstractTableModel;
  * 
  */
 public class EventResultsEditorTableModel extends AbstractTableModel implements EditorTableModel<EventResults> {
-	private EventResults buffer; // this holds the current state of the results, as edited by the user
-	private Facet facet; // the facet edited by this editor
+    private EventResults buffer; // this holds the current state of the results, as edited by the user
+    private Facet facet; // the facet edited by this editor
 
-	@Override
-	public int getColumnCount() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    public int getColumnCount () {
+        // TODO Auto-generated method stub
+        return 0;
+    }
 
-	@Override
-	public int getRowCount() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    public int getRowCount () {
+        // TODO Auto-generated method stub
+        return 0;
+    }
 
-	@Override
-	public Object getValueAt(int rowIndex, int columnIndex) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public Object getValueAt (int rowIndex, int columnIndex) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public int numStages() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    public int numStages () {
+        // TODO Auto-generated method stub
+        return 0;
+    }
 
-	@Override
-	public int currentStage() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    public int currentStage () {
+        // TODO Auto-generated method stub
+        return 0;
+    }
 
-	@Override
-	public void nextStage() {
-		// TODO Auto-generated method stub
+    @Override
+    public void nextStage () {
+        // TODO Auto-generated method stub
 
-	}
+    }
 
-	@Override
-	public void previousStage() {
-		// TODO Auto-generated method stub
+    @Override
+    public void previousStage () {
+        // TODO Auto-generated method stub
 
-	}
+    }
 
-	@Override
-	public EventResults getResult() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public EventResults getResult () {
+        // TODO Auto-generated method stub
+        return null;
+    }
 }
