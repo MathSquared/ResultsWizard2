fafe1d7b67fe28a1588e9263f59503ce446659b5
MathSquared <MathSquared@users.noreply.github.com>
2014-09-06 22:46:50 -0500

    Add Facet
    Sensible parameter to pass into the (NYI) ERETM constructor. We can only
    sensibly edit one facet at a time, so we need a "facet" parameter of
    some sort.

---
+++ ResultsWizard2/src/mathsquared/resultswizard2/Facet.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package mathsquared.resultswizard2;
+
+/**
+ * Represents one portion of {@link EventResults}: individual, team, or special honors.
+ * 
+ * @author MathSquared
+ *
+ */
+public enum Facet {
+	INDIVIDUAL, TEAM, SPECIAL;
+}
