b5f4dd6820e3e7a70fa3f7fddd59dffe3973c4ad
MathSquared <MathSquared@users.noreply.github.com>
2014-09-06 22:56:14 -0500

    Formatting (Facet)
    Newlining after every enum constant is an Eclipse setting I added for
    when I'm working with more crazy enums (with constructors and class
    bodies and all), but I guess it works here too.

--- ResultsWizard2/src/mathsquared/resultswizard2/Facet.java
+++ ResultsWizard2/src/mathsquared/resultswizard2/Facet.java
@@ -7,8 +7,10 @@ package mathsquared.resultswizard2;
  * Represents one portion of {@link EventResults}: individual, team, or special honors.
  * 
  * @author MathSquared
- *
+ * 
  */
 public enum Facet {
-	INDIVIDUAL, TEAM, SPECIAL;
+    INDIVIDUAL,
+    TEAM,
+    SPECIAL;
 }
