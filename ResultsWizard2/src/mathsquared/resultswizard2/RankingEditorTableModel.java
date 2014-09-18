4db322b3a5ca7ae12947d1de0cf0aaf9d6b790ff
MathSquared <MathSquared@users.noreply.github.com>
2014-09-17 22:04:51 -0500

    Implement RETM constructor
    Yes, my comments are a lot more verbose.

--- ResultsWizard2/src/mathsquared/resultswizard2/RankingEditorTableModel.java
+++ ResultsWizard2/src/mathsquared/resultswizard2/RankingEditorTableModel.java
@@ -19,8 +19,41 @@ public abstract class RankingEditorTableModel<T> extends AbstractTableModel impl
     private String[] colNames;
     private Class[] colClasses;
 
+    public RankingEditorTableModel (String[] names, Class[] classes) {
+        // The first elements of colNames and colClasses will be "End tie?" and Boolean.class, respectively.
+        // We copy from names and classes into the rest of colNames and colClasses.
+
+        // Let's do our length sanity check here so we still have a half-functional instance
+        // if names and classes have different lengths
+        // So, if the arguments aren't sane (i.e. the same length), we'll still initialize the array lengths to 1
+        // (this helps mitigate finalizer exploits)
+        if (names.length != classes.length) {
+            colNames = new String[1];
+            colClasses = new Class[1];
+            // Throw the exception later so we initialize the arrays first to counteract finalizer attacks
+        } else {
+            colNames = new String[names.length + 1];
+            colClasses = new Class[classes.length + 1];
+        }
+
+        // Initialize arrays
+        colNames[0] = "End tie?";
+        colClasses[0] = Boolean.class;
+
+        // Now, die horribly if the arg lengths don't match
+        if (names.length != classes.length) {
+            throw new IllegalArgumentException("Length of names and classes must match (names: " + names.length + ", classes: " + classes.length + ")");
+        }
+
+        // Copy the remaining entries from names and classes into colNames and colClasses
+        // String and Class are immutable
+        System.arraycopy(names, 0, colNames, 1, names.length);
+        System.arraycopy(classes, 0, colClasses, 1, classes.length);
+    }
+
     // In theory, colNames and colClasses will have the same lengths.
     // This, my friends, is why RETM is not Serializable.
+    // (don't want hostile streams messing with the array lengths)
     public int getColumnCount () {
         return colNames.length;
     }
