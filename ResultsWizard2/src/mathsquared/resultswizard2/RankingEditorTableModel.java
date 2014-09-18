8ed89b6040400fbae9cd51c013e57369f5e0c863
MathSquared <MathSquared@users.noreply.github.com>
2014-09-17 21:55:27 -0500

    Start working on RankingEditorTableModel
    because why not?

--- ResultsWizard2/src/mathsquared/resultswizard2/RankingEditorTableModel.java
+++ ResultsWizard2/src/mathsquared/resultswizard2/RankingEditorTableModel.java
@@ -13,5 +13,15 @@ import javax.swing.table.AbstractTableModel;
  * 
  */
 public abstract class RankingEditorTableModel<T> extends AbstractTableModel implements EditorTableModel<T> {
+    // These are the columns.
+    // We initialize one for ties and leave the rest to the constructor.
+    // Presumably, the constructor will be invoked by a subclass using super.
+    private String[] colNames;
+    private Class[] colClasses;
 
+    // In theory, colNames and colClasses will have the same lengths.
+    // This, my friends, is why RETM is not Serializable.
+    public int getColumnCount () {
+        return colNames.length;
+    }
 }
